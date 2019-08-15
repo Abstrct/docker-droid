dfc36e68ee4bd436d83a8de28322eade48b9560c
G4te-Keep3r <juliettaylorswift@gmail.com>
2019-08-15 02:21:38 -0500

    fixed defcoind
    defcoind refers to conf file again. if removed to deal with later.

--- droid/droid.sh
+++ droid/droid.sh
@@ -8,38 +8,38 @@ echo 'Defcoind started with a snapshot, so it will sync a lot faster than normal
 
 
 # Check to see if the wallet is finally sync'd
-if [ ! -f "/src/droid/bootstrap_done.txt" ]; then
-	progress=`/src/droid/client/bin/defcoin-cli -conf=/src/droid/client/data/defcoin.conf getblockchaininfo | jq -r '.verificationprogress'`
-	progress=`awk "BEGIN { print ${progress} + 0.01 }"`
-	echo 'Checking on wallet status, must be up to date before starting bot' >> /src/droid/logs.txt
-	echo "Current Defcoin Sync progress: ${progress}" >> /src/droid/logs.txt
-	while (( $(echo "${progress} < 0.98" | bc -l ) ))
-	do
-	    progress=`/src/droid/client/bin/defcoin-cli -conf=/src/droid/client/data/defcoin.conf getblockchaininfo | jq -r '.verificationprogress'`
-	    progress=`awk "BEGIN { print ${progress} + 0.01 }"`
-	    echo "Sync Progress: ${progress} (This takes a long time and may even look like it's going backwards)" >> /src/droid/logs.txt
-	    sleep 1m
-	done
-	echo 'Sync complete' >> /src/droid/logs.txt
-	echo "bootstrap imported" > /src/droid/bootstrap_done.txt
-else
-	echo "###NOT USING BOOTSTRAP###" >> /src/droid/logs.txt
-	progress=`/src/droid/client/bin/defcoin-cli getblockchaininfo | jq -r '.verificationprogress'`
-	progress=`awk "BEGIN { print ${progress} + 0.01 }"`
-	echo 'Checking on wallet status, must be up to date before starting bot' >> /src/droid/logs.txt
-	echo "Current Defcoin Sync progress: ${progress}" >> /src/droid/logs.txt
-	while (( $(echo "${progress} < 0.98" | bc -l ) ))
-	do
-	    progress=`/src/droid/client/bin/defcoin-cli getblockchaininfo | jq -r '.verificationprogress'`
-	    progress=`awk "BEGIN { print ${progress} + 0.01 }"`
-	    echo "###NOT USING BOOTSTRAP### Sync Progress: ${progress} (This takes a long time and may even look like it's going backwards)" >> /src/droid/logs.txt
-	    echo "###NOT USING BOOTSTRAP###" >> /src/droid/logs.txt
-	    sleep 1m
-	done
-	echo "###NOT USING BOOTSTRAP###" >> /src/droid/logs.txt
-	echo 'Sync complete' >> /src/droid/logs.txt
-	echo "bootstrap imported" > /src/droid/bootstrap_done.txt
-fi
+#if [ ! -f "/src/droid/bootstrap_done.txt" ]; then
+progress=`/src/droid/client/bin/defcoin-cli -conf=/src/droid/client/data/defcoin.conf getblockchaininfo | jq -r '.verificationprogress'`
+progress=`awk "BEGIN { print ${progress} + 0.01 }"`
+echo 'Checking on wallet status, must be up to date before starting bot' >> /src/droid/logs.txt
+echo "Current Defcoin Sync progress: ${progress}" >> /src/droid/logs.txt
+while (( $(echo "${progress} < 0.98" | bc -l ) ))
+do
+    progress=`/src/droid/client/bin/defcoin-cli -conf=/src/droid/client/data/defcoin.conf getblockchaininfo | jq -r '.verificationprogress'`
+    progress=`awk "BEGIN { print ${progress} + 0.01 }"`
+    echo "Sync Progress: ${progress} (This takes a long time and may even look like it's going backwards)" >> /src/droid/logs.txt
+    sleep 1m
+done
+echo 'Sync complete' >> /src/droid/logs.txt
+echo "bootstrap imported" > /src/droid/bootstrap_done.txt
+#else
+#	echo "###NOT USING BOOTSTRAP###" >> /src/droid/logs.txt
+#	progress=`/src/droid/client/bin/defcoin-cli -conf=/src/droid/client/data/defcoin.conf getblockchaininfo | jq -r '.verificationprogress'`
+#	progress=`awk "BEGIN { print ${progress} + 0.01 }"`
+#	echo 'Checking on wallet status, must be up to date before starting bot' >> /src/droid/logs.txt
+#	echo "Current Defcoin Sync progress: ${progress}" >> /src/droid/logs.txt
+#	while (( $(echo "${progress} < 0.98" | bc -l ) ))
+#	do
+#	    progress=`/src/droid/client/bin/defcoin-cli -conf=/src/droid/client/data/defcoin.conf getblockchaininfo | jq -r '.verificationprogress'`
+#	    progress=`awk "BEGIN { print ${progress} + 0.01 }"`
+#	    echo "###NOT USING BOOTSTRAP### Sync Progress: ${progress} (This takes a long time and may even look like it's going backwards)" >> /src/droid/logs.txt
+#	    echo "###NOT USING BOOTSTRAP###" >> /src/droid/logs.txt
+#	    sleep 1m
+#	done
+#	echo "###NOT USING BOOTSTRAP###" >> /src/droid/logs.txt
+#	echo 'Sync complete' >> /src/droid/logs.txt
+#	echo "bootstrap imported" > /src/droid/bootstrap_done.txt
+#fi
 
 
 
